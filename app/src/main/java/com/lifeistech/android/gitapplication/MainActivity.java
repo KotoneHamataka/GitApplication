718e9c2794ea099709957b02a3726831e2546e77
Kotone Hamataka <kochiii@hotmail.co.jp>
2016-02-15 19:02:50 +0900

    int 10

--- app/src/main/java/com/lifeistech/android/gitapplication/MainActivity.java
+++ app/src/main/java/com/lifeistech/android/gitapplication/MainActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    int number =0;
+    int number = 10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
